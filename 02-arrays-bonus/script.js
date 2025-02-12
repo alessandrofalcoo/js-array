a06ec616df9d48db489ee1c0ed44d72ff1076f08
alessandrofalcoo <alefalco.1003@gmail.com>
2025-02-12 16:43:00 +0100

    fix exercise n2

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -16,7 +16,7 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = teachers;
+const longNames = teachers.filter(name => name.length >= 5);
 console.log(longNames);
 
 
